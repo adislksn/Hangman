d7beb897b4107f8caa5ad79992c31021b477ed5c
Pandu19 <pandu.120140020@student.itera.ac.id>
2022-05-07 13:41:25 +0700

    Change Language

--- Hangman-code.py
+++ Hangman-code.py
@@ -3,7 +3,7 @@ import pygame
 import math
 import random as rand
 
-#display settings
+#display & music settings
 pygame.init()
 logo = pygame.image.load("assets/hangman6.png")
 pygame.display.set_icon(logo)
@@ -14,14 +14,14 @@ width = 800
 height = 500
 display = pygame.display.set_mode((width, height))
 background = (255,255,255)
-font_dasar = pygame.font.SysFont("comicsans",32)
+base_font = pygame.font.SysFont("comicsans",32)
 clock = pygame.time.Clock()
 pygame.display.set_caption("Hangman")
 
-#tombol
+#button
 radius = 20
 gap = 15
-alfabet = []
+alphabet = []
 startx = 550
 starty = 50
 for i in range(26):
@@ -31,33 +31,34 @@ for i in range(26):
     else:
         x = startx + gap * 2 + ((radius * 2 + gap) * ((i+1) % 4))
         y = starty + (((i+1) // 4) * (gap + radius * 2))
-    alfabet.append([x, y, chr(65 + i), True])
+    alphabet.append([x, y, chr(65 + i), True])
 
-#gambar
+#images
 images=[]
 for i in range(7):
     image = pygame.image.load(f"assets/hangman{str(i)}.png")
     images.append(image)
 
 #assets
-list_text=(['AYAM','KUCING','BERUANG','BADAK','HARIMAU','KELINCI','KEPITING'],['APEL','PISANG','KENTANG','DAGING','JERUK','COKELAT','KUE'],['YOUTUBER','PROGRAMMER','POLISI','DOKTER','PILOT','CHEF','DRIVER'])
+list_text=(['CHICKEN','CAT','BEAR','BIRD','TIGER','RABBIT','ZEBRA'],['APPLE','BANANA','POTATO','MEAT','ORANGE','CHOCOLATE','CAKE'],['YOUTUBER','PROGRAMMER','POLICE','DOCTOR','PILOT','CHEF','DRIVER'])
 stats = 0
-terjawab=[]
+answered=[]
 
+#parent class
 class Hangman:
-    global quest
+    global quest #variabel quest bermasalah, blm fix
     quest = rand.choice(list_text[rand.randint(0,2)])
-    def __init__(self,nama):
-        self.nama = nama
+    def __init__(self,name):
+        self.name = name
 
     def close():
         pygame.quit()
 
-    def tampilan_out(self,word):
+    def out_display(self,word):
         self.word = word
         pygame.time.delay(1000)
         display.fill(background)
-        text = font_dasar.render(word, 1, (0,0,0))
+        text = base_font.render(word, 1, (0,0,0))
         display.blit(text, (width/2 - text.get_width()/2, height/2 - text.get_height()/2))
         pygame.display.update()
         pygame.time.delay(3000)
@@ -67,32 +68,33 @@ class Hangman:
         display.fill(background)
 
         #draw title
-        text = font_dasar.render("Hangman Game", 1, (0,0,0))
+        text = base_font.render("Hangman Game", 1, (0,0,0))
         display.blit(text, (width/2 - text.get_width()/2, 20))
         
         #draw words
-        show_alfabet = ""
-        for teks in quest:
-            if teks in terjawab:
-                show_alfabet += teks + " "
+        show_alphabet = ""
+        for check in quest:
+            if check in answered:
+                show_alphabet += check + " "
             else:
-                show_alfabet += "_ "
-        text = font_dasar.render(show_alfabet, 1, (0,0,0))
+                show_alphabet += "_ "
+        text = base_font.render(show_alphabet, 1, (0,0,0))
         display.blit(text, (200, 400))
 
         #draw buttons
-        for teks in alfabet:
-            x, y, txt, status = teks
+        for check in alphabet:
+            x, y, txt, status = check
             if status:
                 pygame.draw.circle(display, (0,0,0), (x, y), radius,3)
-                text = font_dasar.render(txt, 1, (0,0,0))
+                text = base_font.render(txt, 1, (0,0,0))
                 display.blit(text, (x - text.get_width()/2, y - text.get_height()/2))
         
         #draw images    
         display.blit(images[stats], (150,100))
         pygame.display.update()
 
-    def utama(self):
+    #main program
+    def main_p(self):
         global stats
         while True:
             clock.tick(60)
@@ -102,77 +104,77 @@ class Hangman:
                 elif event.type == pygame.MOUSEBUTTONDOWN:
                     pos_x, pos_y = pygame.mouse.get_pos()
                     click.play()
-                    for teks in alfabet:
-                        x, y, txt, status = teks
+                    for check in alphabet:
+                        x, y, txt, status = check
                         if status:
                             cek = math.sqrt((x - pos_x)**2 + (y - pos_y)**2)
                             if cek <= radius:
-                                teks[3] = False
-                                terjawab.append(txt)
-                                print(terjawab)
+                                check[3] = False
+                                answered.append(txt)
+                                print(answered)
                                 if txt not in quest:
                                     stats += 1
             Hangman.show()
 
             game = True
-            for teks in alfabet:
-                if teks not in terjawab:
+            for check in alphabet:
+                if check not in answered:
                     game = False
                     break
             
             if game:
-                Hangman.tampilan_out(self,"You Won")
+                Hangman.out_display(self,"You Won")
                 break
             if stats == 6:
-                Hangman.tampilan_out(self,"You Lost")
+                Hangman.out_display(self,"You Lost")
                 break
 
 class Animal(Hangman):
-    quest = rand.choice(list_text[1])
-    def __init__(self,nama):
-        super().__init__(nama)
-
-    def tampilan_out(self,word):
-        super().tampilan_out(word)
+    quest = rand.choice(list_text[0])
+    def __init__(self,name):
+        super().__init__(name)
+    
+    def out_display(self,word):
+        super().out_display(word)
     
     def show():
         super().show()
 
-    def utama(self):
-        super().utama()
+    def main_p(self):
+        super().main_p()
 
 class Food(Hangman):
-    quest = rand.choice(list_text[2])
-    def __init__(self,nama):
-        super().__init__(nama)
+    quest = rand.choice(list_text[1])
+    def __init__(self,name):
+        super().__init__(name)
 
-    def tampilan_out(self,word):
-        super().tampilan_out(word)
+    def out_display(self,word):
+        super().out_display(word)
     
     def show():
         super().show()
 
-    def utama(self):
-        super().utama()
+    def main_p(self):
+        super().main_p()
 
 class Jobs(Hangman):
-    quest = rand.choice(list_text[0])
-    def __init__(self,nama):
-        super().__init__(nama)
+    quest = rand.choice(list_text[2])
+    def __init__(self,name):
+        super().__init__(name)
 
-    def tampilan_out(self,word):
-        super().tampilan_out(word)
+    def out_display(self,word):
+        super().out_display(word)
     
     def show():
         super().show()
 
-    def utama(self):
-        super().utama()
+    def main_p(self):
+        super().main_p()
         
 display.fill(background)
 pygame.display.update()
 play = "Player 1 "
-player=Hangman(play)
-player.utama()
+player=Food(play)
+player.main_p()
 
 pygame.quit()
